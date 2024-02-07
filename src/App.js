7072c69e6f23c71bb58630c8aec6d47a0f55c4e4
mahendra <mandhatimahendra1178@gmail.com>
2024-02-07 13:13:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+
+class App extends Component{
+
+  state={isHide:true,isSecond:true}
+
+  onClickFirst=()=>{
+    this.setState(prevState=>({isHide:!prevState.isHide}))
+  }
+
+  onClickSecond=()=>{
+    this.setState(prevState=>({isSecond:!prevState.isSecond}))
+  }
+
+  render(){
+    const {isHide}=this.state
+    const {isSecond}=this.state
+
+    const first=isHide?"hide-first":"first";
+    const second=isSecond?"hide-second":"second";
+    return (<div className="bg-container">
+      <h1>Show/Hide</h1>
+      <div className="buttons-container">
+        <div className="container-1">
+           <button className="button" onClick={this.onClickFirst}>Show/Hide FirstName</button>
+           <div >
+            <h1 className={first}>Joe</h1>
+           </div>
+        </div>
+        <div className="container-1">
+           <button className="button" onClick={this.onClickSecond}>Show/Hide LastName</button>
+           <div >
+            <h1 className={second}>Jonas</h1>
+           </div>
+
+        </div>
+        
+        
+      </div>
+
+    </div>)
+  }
 }
 
 export default App;
